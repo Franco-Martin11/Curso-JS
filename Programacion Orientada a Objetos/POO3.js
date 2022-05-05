50350a43d4b82de3de87308ff3a13404cae97bef
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-05-05 17:33:18 -0300

    POO3(Metodos 'static','get' y 'set')

--- Programacion Orientada a Objetos/POO3.js
+++ Programacion Orientada a Objetos/POO3.js
@@ -23,21 +23,37 @@ class Humano {
     this.nombre = nombre;
     this.altura = altura;
     this.edad = null;
+    this.colorDePelo = null;
   }
 
   //-> Getter : Metodo obtenedor
-  get getEdad() {
-    return this.edad;
+  get getGetter() {
+    return this.edad, this.colorDePelo;
   }
+
+  // La funcion del getter es enlazar los nuevos valores dados por el setter, retornalos a la instanacia principal y ubicarlos en el valor de los atributos correspondientes.
+
   //-> Setter : Metodo colocador
   set setEdad(edad) {
     this.edad = edad;
+  }
+
+  //La funcion del setter es obtener los valores correspondientes a los atributos de dicha instancia.
+
+  set setColorDePelo(colorDePelo) {
+    this.colorDePelo = colorDePelo;
   }
 }
 
 const franco1 = new Humano("franco", 1.78);
 console.log(franco1);
 
+// Los valores de los metodos 'get'y 'set' se expresan en forma de (var:value) como si se tratara de un atributo del objeto.
+
 franco1.setEdad = 23;
 
-console.log(franco1);
+//console.log(franco1);
+
+franco1.colorDePelo = "azul";
+
+//console.log(franco1);
