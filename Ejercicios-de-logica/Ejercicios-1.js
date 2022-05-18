5818466ea056f15b45b062561a796fb6d7f1e6f9
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-05-17 22:08:58 -0300

    Ejecicio-1(funcion 8)

--- Ejercicios-de-logica/Ejercicios-1.js
+++ Ejercicios-de-logica/Ejercicios-1.js
@@ -115,4 +115,25 @@ const Palindromo = (palabra) => {
   return palindromo;
 };
 
+//*Ejecicio 8
+
+//? 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+
+
+
+const ReplaceFunc = (elm1, ...elm2) => {
+  const entrada = [elm1, ...elm2];
+  const replace = [];
+
+  entrada.forEach((element, index) => {
+    replace[index] = entrada[index].replace("asd", "");
+    replace[index] === ""
+      ? console.warn(`el elemento del indice ${index} es un 'asd'`)
+      : console.log(replace[index]);
+  });
+
+ // console.log(replace);
+};
+
+// ReplaceFunc("asd", "asd2", "asdc4", "ascd5");
 
