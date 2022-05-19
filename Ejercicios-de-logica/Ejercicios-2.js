92a0a85308a6633d49c8adec26831f87de5c22cd
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-05-19 18:46:58 -0300

    Ejercicios-2.js(11;14'terminados')

--- Ejercicios-de-logica/Ejercicios-2.js
+++ Ejercicios-de-logica/Ejercicios-2.js
@@ -36,4 +36,54 @@ const Factorial = (factor) => {
   console.log(factorialResult);
 };
 
-Factorial(4);
+//* Ejercicio-3
+
+/*
+? 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
+? 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+? 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
+*/
+
+const ConversorTemp = (temp, tipo) => {
+  if (!temp || !tipo) {
+    console.warn("complete ambas casillas");
+  } else if ((temp !== Number, tipo !== String)) {
+    console.warn("Coloque los datos correctamente");
+  }
+
+  if (tipo === "C") {
+    let resultado = (temp * 9) / 5 + 32;
+    return resultado;
+  } else if (tipo === "F") {
+    let resultado = ((temp - 32) * 5) / 9;
+    return resultado;
+  } else {
+    console.warn("coloque un tipo de grados correcto , F o C");
+  }
+};
+
+//* (1)
+const Primo = (num) => {
+  let contador = 0;
+
+  for (let index = 1; index <= num; index++) {
+    if (num % index === 0) {
+      contador++;
+    }
+  }
+
+  contador === 2
+    ? console.info(`El numero ${num} es un numero primo`)
+    : console.warn(`El numero ${num} no es un numero primo`);
+};
+
+//* (2)
+const Par = (numero) => {
+  !numero
+    ? console.warn("No ingresastes un numero")
+    : numero % 2 === 0
+    ? console.log(`El numero ${numero} es par`)
+    : console.warn(`El numero ${numero} no es par`);
+};
+
+//*(3)
