5b26ab33655ead22ef731f0e5fb05096862d1c21
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-05-19 18:08:03 -0300

    Ejercicios-2.js(9;11'terminados')

--- Ejercicios-de-logica/Ejercicios-2.js
+++ Ejercicios-de-logica/Ejercicios-2.js
@@ -6,3 +6,34 @@
  * ? 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
  */
 
+const numAleatorio = (min, max) => {
+  let number = Math.random() * (max - min) + min;
+  number = Math.floor(number);
+  return number;
+};
+
+const capicua = (numero) => {
+  if (!numero) {
+    console.warn("No ingresaste un numero");
+  } else if (numero >= 0 && numero <= 9) {
+    console.warn("El numero es de una sola cifra");
+  } else {
+    let numeroAnalizado = numero.toString();
+    let numeroAnalizar = numero.toString().split("").reverse().join("");
+    if (numeroAnalizar === numeroAnalizado) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+const Factorial = (factor) => {
+  let factorialResult = 1;
+  for (let index = 1; index <= factor; index++) {
+    factorialResult = index * factorialResult;
+  }
+  console.log(factorialResult);
+};
+
+Factorial(4);
