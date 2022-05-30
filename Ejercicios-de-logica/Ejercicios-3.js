8bbd683143221db21850d53f3a744f937b0e7a28
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-05-29 22:24:56 -0300

    Ejercicios 21:23(Terminados)

--- Ejercicios-de-logica/Ejercicios-3.js
+++ Ejercicios-de-logica/Ejercicios-3.js
@@ -17,39 +17,40 @@ const Potencia = () => {
 
 //? 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
 
-/*
-! const NumeroMaximo = () => {
-  const numbersArrayMaximo = [1, 4, 5, 99, -60];
-  let NumeroMinimo;
-  let NumeroMaximo;
+const NumeroMaximo = (array) => {
+  if (array === undefined) return console.warn("no ingresaste un arreglo");
 
-  for (let index = 0; index < numbersArrayMaximo.length; index++) {
-    if (!NumeroMaximo) {
-      NumeroMaximo = numbersArrayMaximo[index];
-    } else if (NumeroMaximo >= numbersArrayMaximo[index + 1]) {
-      let numeroOrden = NumeroMaximo;
-      numbersArrayMaximo[index] = numbersArrayMaximo[index + 1];
-      numbersArrayMaximo[index + 1] = numeroOrden;
-      numeroOrden = NumeroMaximo;
-    }
+  if (array.length === 0) return console.warn("el array esta vacio");
+
+  for (let element of array) {
+    if (typeof element !== "number")
+      return console.warn(`el elemento ${element} no es un numero`);
   }
-  return NumeroMaximo;
+
+  return console.info(
+    `Arreglo Original: ${array}\nValor Mayor:${Math.max(
+      ...array
+    )}\nValor Menor:${Math.min(...array)}`
+  );
 };
-*/
+
+//!NumeroMaximo(arrayPI)
 
 //? 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
+const arrayPI = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
 const ParesImpares = () => {
-  const arrayPI = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const arrayPares = [];
   const arrayImpares = [];
 
   const Metodo = (element) => {
     element % 2 === 0 ? arrayPares.push(element) : arrayImpares.push(element);
   };
+
   arrayPI.forEach((element) => {
     Metodo(element);
   });
+
   console.log({ Pares: [...arrayImpares] }, { Impares: [...arrayPares] });
 };
-
