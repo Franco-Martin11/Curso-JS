115f82659523087d2e64d2e8f8684655016d0fea
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-06-03 17:34:14 -0300

    Ejercicios-4(24)

--- Ejercicios-de-logica/Ejercicios-4.js
+++ Ejercicios-de-logica/Ejercicios-4.js
@@ -7,7 +7,7 @@
 *La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
   ? - Todos los datos del objeto son obligatorios.
 
-  ? - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 7 restantes números.
+  ?* - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 7 restantes números.
 
   ? - Valida que el título no rebase los 100 caracteres.
 
@@ -37,7 +37,7 @@
 
 class Pelicula {
   constructor({
-    IMBD,
+    id = null,
     titulo,
     director,
     añoEstreno,
@@ -45,59 +45,86 @@ class Pelicula {
     generos,
     calificacion,
   }) {
-    this.IMBD = IMBD;
+    this.IMBD = id;
     this.titulo = titulo;
     this.director = director;
     this.añoEstreno = añoEstreno;
     this.paises = pais;
     this.generos = generos;
     this.calificacion = calificacion;
-
-    this.validarIMDB(IMBD);
+    this.validarId(id);
   }
 
   //* OP.Ternario tiene un 'return' IMPLICITO
-
+  //? Bloque de abstracciones
   validarString(propiedad, valor) {
-    !valor ? console.warn(`La ${propiedad} "${valor}" esta vacio `) : "";
+    if (!valor) {
+      return console.warn(`La ${propiedad} "${valor}" esta vacio `);
+    }
+    if (typeof valor !== "string") {
+      return console.error("El dato ingresado no es una cadena de texto");
+    } else {
+      return true;
+    }
+  }
 
-    typeof valor !== "string"
+  validarTamañoString(propiedad, value, string) {
+    string > value
       ? console.error(
-          `${propiedad} "${valor}" ingresado, No es una cadena de texto`
+          `La propiedad ${propiedad} debe tener como maximo "${value}" caracteres`
         )
       : true;
   }
 
+  validarNumInt(propiedad, value) {
+    typeof value !== "number" || value.length !== 4
+      ? console.error(
+          `El valor de ${propiedad} debe ser un numero de 4 digitos`
+        )
+      : true;
+  }
+
+  validarArray(propiedad, value) {
+    !Array.isArray(value)
+      ? console.error(`${propiedad} debe ser un arreglo`)
+      : true;
+  }
+
+  validarGeneros(propiedad, genero, value) {
+    let resultado;
+    let errores = [];
+    value.forEach((element) => {
+      resultado = element.includes(genero);
+      resultado === false ? errores.push(element) : true;
+    });
+    if (!resultado) {
+      console.error(
+        `La ${propiedad} no esta incluida en los generos acceptados `
+      );
+      console.warn(errores);
+    }
+  }
   //? Valida el IMBD utilizando el metodo 'validarString()' el cual solo se procesa si 'validarString' retorna 'true', sino da el 'console.warn()'
 
-  validarIMDB(IMBD) {
-    if (this.validarString("IMDB id", IMBD)) {
-      !/^(a-z)[2](0-9)[7]$/.test(IMBD)
-        ? console.warn(
-            `IMDB id "${IMBD}" no es valido, debe tener 9 caracteres, los 2 primero letras minusculas,los 7 restantes numeros`
-          )
-        : "";
+  validarId(id) {
+    if (this.validarString("IMDB id", id) === true) {
+      if (!/^([a-z]){2}([0-9]){7}$/.test(id)) {
+        return console.error(
+          `IMDB id : "${id}" no es valido, debe tener 9 caracteres , los primeros 2 letras minusculas, los 7 restantes numeros`
+        );
+      }
     }
   }
 }
-const generosAceptador = [
-  "thriller",
-  "suspense",
-  "drama",
-  "animation",
-  "fiction",
-];
-function getLengthObject(objeto) {
-  let lengthObj = Object.keys(objeto).length;
-  return lengthObj;
-}
 
-const peliculaNueva = new Pelicula({
-  IMBD: "ff1234567",
-  titulo: "hola que tal",
-  director: "arnold schwarseneger",
-  añoEstreno: 2021,
-  pais: "argentina",
-  generos: "thriller",
-  calificacion: 7,
-});
+const nuevaPelicula = new Pelicula({ id: "" });
+
+function prueba(getArray, Array) {
+  let resultado;
+  for (let i = 0; i < getArray.length; i++) {
+    Array.forEach((element) => {
+      return resultado = element === getArray[i];
+    });
+  }
+  console.log(resultado)
+}
